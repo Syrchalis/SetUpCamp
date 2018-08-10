c0901e113961bf9c6dd6e7341735520405a23a54
Syrchalis <41159904+Syrchalis@users.noreply.github.com>
2018-08-10 21:34:28 +0200

    Add files via upload

--- Source/SetupCamp/ModSettings.cs
+++ Source/SetupCamp/ModSettings.cs
@@ -17,6 +17,8 @@ namespace Syrchalis_SetUpCamp
         private const int MinSizeDefault = 75;
         private const int MaxSizeDefault = 125;
         public static bool permanentCamps = false;
+        public static bool homeEvents = false;
+        public static bool caravanEvents = false;
         public static IntRange allowedSizeRange = new IntRange(50, 300);
 
         public override void ExposeData()
@@ -24,8 +26,21 @@ namespace Syrchalis_SetUpCamp
             base.ExposeData();
             Scribe_Values.Look<IntRange>(ref allowedSizeRange, "allowedSizeRange", new IntRange(75, 125), true);
             Scribe_Values.Look<bool>(ref permanentCamps, "permanentCamps", false, true);
+            Scribe_Values.Look<bool>(ref homeEvents, "homeEvents", false, true);
+            Scribe_Values.Look<bool>(ref caravanEvents, "caravanEvents", false, true);
         }
     }
+
+    [DefOf]
+    public static class SetUpCampDefOf
+    {
+        static SetUpCampDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(SetUpCampDefOf));
+        }
+        public static WorldObjectDef CaravanCamp;
+    }
+
     public class SetUpCamp : Mod
     {
         public static SetUpCampSettings settings;
@@ -52,13 +67,43 @@ namespace Syrchalis_SetUpCamp
             listing_Standard.Label("SetUpCampSettingMapSize".Translate());
             listing_Standard.IntRange(ref SetUpCampSettings.allowedSizeRange, 50, 300);
             listing_Standard.AddLabeledCheckbox("SetUpCampSettingsPermanentCamps".Translate() + ": ", ref SetUpCampSettings.permanentCamps);
+            listing_Standard.AddLabeledCheckbox("SetUpCampSettingsHomeEvents".Translate() + ": ", ref SetUpCampSettings.homeEvents);
+            listing_Standard.AddLabeledCheckbox("SetUpCampSettingsCaravanEvents".Translate() + ": ", ref SetUpCampSettings.caravanEvents);
             listing_Standard.End();
             settings.Write();
+        }
+
+        public static void ApplySettings()
+        {
+            if (SetUpCampSettings.homeEvents)
+            {
+                SetUpCampDefOf.CaravanCamp.IncidentTargetTags.Add(IncidentTargetTagDefOf.Map_PlayerHome);
+            }
+            else
+            {
+                SetUpCampDefOf.CaravanCamp.IncidentTargetTags.Remove(IncidentTargetTagDefOf.Map_PlayerHome);
+            }
+            if (SetUpCampSettings.caravanEvents)
+            {
+                SetUpCampDefOf.CaravanCamp.IncidentTargetTags.Add(IncidentTargetTagDefOf.Caravan);
+            }
+            else
+            {
+                SetUpCampDefOf.CaravanCamp.IncidentTargetTags.Remove(IncidentTargetTagDefOf.Caravan);
+            }
+            SetUpCampDefOf.CaravanCamp.ResolveReferences();
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            ApplySettings();
         }
 
         public static void GetGizmosPostfix(ref IEnumerable<Gizmo> __result, Caravan __instance)
         {
             __result = __result.Concat(new Gizmo[] { SetUpCampCommand(__instance) });
+            ApplySettings();
         }
 
         private static StringBuilder tmpSettleFailReason = new StringBuilder();
