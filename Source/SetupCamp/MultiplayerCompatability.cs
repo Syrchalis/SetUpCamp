45228ceb80031e4b453119f4d282e989b9987f8d
Thomas107500 <hotsunngai@gmail.com>
2020-06-20 14:15:32 +0800

    Added multiplayer compatability

---
+++ Source/SetupCamp/MultiplayerCompatability.cs
@@ -0,0 +1,26 @@
+﻿using Multiplayer.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Syrchalis_SetUpCamp
+{
+    class MultiplayerCompatability
+    {
+        [StaticConstructorOnStartup]
+        static class MultiplayerCompatibility
+        {
+            static MultiplayerCompatibility()
+            {
+                if (!MP.enabled) return;
+
+                MP.RegisterSyncMethod(typeof(SetUpCamp_Utility), nameof(SetUpCamp_Utility.Camp));
+                MP.RegisterSyncField(typeof(SetUpCamp_Utility), nameof(SetUpCamp_Utility.randomSeed));
+
+            }
+        }
+    }
+}
